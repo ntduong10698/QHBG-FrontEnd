55255487f1e794d1e7afd477e5c88c284478106f
Do Xuan Hieu <anhhieuaxy@gmail.com>
2019-10-21 23:36:43 +0700

    code 11h36

--- target/classes/static/js/ajax/ajax_infoDecision.js
+++ target/classes/static/js/ajax/ajax_infoDecision.js
@@ -2,6 +2,7 @@ var idWed = window.location.href.split("=");
 $(document).ready(function () {
     getDataInfoQuyetDinh();
     // getDataDieuChinhBoSung();
+    getQuyetDinhLienQuan(idWed[idWed.length - 1]);
 });
 
 function getDataInfoQuyetDinh() {
@@ -89,6 +90,86 @@ function getDataInfoQuyetDinh() {
         }
 
 
+    });
+}
+
+function getQuyetDinhLienQuan(id) {
+//     ajaxCallGet("v1/public/quyet-dinh/ho-so-kem-theo/find-by-quyet-dinh?id=" + id).then(data => {
+//         console.log(data);
+//         if (data.length > 0) {
+//             let tmp = "";
+//             data.map(function (result,index) {
+//                 tmp += `
+// <tr>
+//                                 <td style="text-align:center;width:5%">${index+1}</td>
+//                                 <td style="width:25%">Thôn Hòa Mục, xã Mỹ An, huyện Lục Ngạn</td>
+//                                 <td style="width:30%">Điều chỉnh diện tích khoảng 1,98 ha đất (đất nuôi trồng
+//                                     thủy sản khoảng 0,86 ha, đất trồng cây lâu năm khoảng 1,12 ha) sang đất cơ
+//                                     sở sản xuất phi nông nghiệp
+//                                 </td>
+//                                 <td style="width:30%">Căn cứ công văn số 1388/UBNN-ĐT ngày 9/11/2017 của Chủ
+//                                     tịch UBND tỉnh về việc cho phép thực hiện dự án đầu tư của công ty cổ phần
+//                                     Clever
+//                                 </td>
+//                                 <td style="width:10%; text-align:center">198,000 m<sup class="sup-text">2</sup>
+//                                 </td>
+//                             </tr>
+// `;
+//             })
+//
+//         }
+//     });
+    ajaxCallGet("v1/public/quyet-dinh/thong-tin-dieu-chinh-quy-hoach/find-by-quyet-dinh?id=" + id).then(data => {
+        console.log(data);
+        if (data.length > 0) {
+            let tmp = "";
+            data.map(function (result, index) {
+                tmp += `
+<tr>
+                                <td style="text-align:center;width:5%">${index + 1}</td>
+                                <td style="width:25%">${result.viTri}</td>
+                                <td style="width:30%">${result.noiDung}</td>
+                                <td style="width:30%">${result.lyDo}</td>
+                                <td style="width:10%; text-align:center">${formatNumber(result.dienTich, '.', '.')}<sup class="sup-text">2</sup>
+                                </td>
+                            </tr>
+`;
+            })
+            $("#quyetDinhLienQuan thead").html("  <th>STT</th>\n" +
+                "            <th>Vị trí, địa điểm</th>\n" +
+                "            <th>Nội dung điều chỉnh, bổ sung</th>\n" +
+                "            <th>Lý do điều chỉnh, bổ sung</th>\n" +
+                "            <th>Diện tích</th>")
+
+            $("#quyetDinhLienQuan tbody").html(tmp)
+        } else {
+            $("#quyetDinhLienQuan tbody").html("<tr><td colspan='13'>Không có dữ liệu để hiển thị</td></tr>");
+        }
+    });
+    ajaxCallGet("v1/public/quyet-dinh/vi-tri-gia-dat-ap-dung/find-by-quyet-dinh?id=" + id).then(data => {
+        console.log(data);
+        if (data.length > 0) {
+            let tmp = "";
+            data.map(function (result, index) {
+                tmp += `
+<tr>
+                                <td style="text-align:center;width:5%">${index + 1}</td>
+                                <td style="width:25%">${result.viTri}</td>
+                                <td style="width:30%">${result.moTa}</td>
+                                <td style="width:30%">${result.lyDo}</td>
+                                <td style="width:10%; text-align:center">${formatNumber(result.giaDat, '.', '.')}<sup class="sup-text">2</sup>
+                                </td>
+                            </tr>
+`;
+            })
+            $("#quyetDinhLienQuan thead").html(" <th>STT</th>\n" +
+                "                                <th>Vị trí, địa điểm</th>\n" +
+                "                                <th>Mô tả</th>\n" +
+                "                                <th>Diện tích</th>");
+            $("#quyetDinhLienQuan tbody").html(tmp)
+        } else {
+            $("#quyetDinhLienQuan tbody").html("<tr><td colspan='13'>Không có dữ liệu để hiển thị</td></tr>");
+        }
     });
 }
 
